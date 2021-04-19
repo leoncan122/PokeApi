c08e65e814ef03b5d29159025b2e9c14f6c42c9f
leoncan122 <leon.can122@gmail.com>
2021-04-19 20:54:17 +0200

    homework

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var nameAndId = document.querySelector('#poke-name-id')
 
 //imagen
 var pokeImage = document.querySelector('#pokeImg')
+pokeImage.src = ''
+
 var evolucionImg = document.querySelector('#poke-evolucion')
 
 
@@ -21,37 +23,55 @@ var newSearch = document.querySelector('#new-search')
 //botones
 var searchPokemonBtn = document.querySelector('#searchBtn')
 
-let intervalId = -1
+var intervalId = -1
+var index = 0
+//funciones 
+
 
 searchPokemonBtn.addEventListener('click', (event) => {
     event.preventDefault();
+    pokeImage.src = 'https://w7.pngwing.com/pngs/235/985/png-transparent-pokemon-red-and-blue-pokemon-snap-pokemon-diamond-and-pearl-pokemon-go-pokemon-let-s-go-eevee-pokemon-go.png'
+    // var images = ['https://c0.klipartz.com/pngpicture/993/363/gratis-png-pokeball.png',
+    // 'https://c0.klipartz.com/pngpicture/682/499/gratis-png-ilustracion-de-pokemon-pikachu-pokemon-platino-pokemon-blanco-y-negro-pokemon-corazon-de-oro-y-almendra-pikachu-ash-ketchum-pikachu.png']    
+    //startGame(images)
     
-
     var url = (`https://pokeapi.co/api/v2/pokemon/${newSearch.value}`)
+
+    function startGame(images) {
+        pokeImage.src = images[index]
+        pokeImage.width = '25px'
+        pokeImage.src = images[1]?  (changeImage()) : index++  
+        index++
+    }
     
 
     fetch(url)
     .then(response => response.json())
     .then( pokemon => {
         
+
         //change values
-        var changeImage = function() {
+
+    var changeImage = function() {
              
-            let index = 0
-            let imagesURL = Object.values(pokemon.sprites)
-            let images = [imagesURL[6],imagesURL[2]]
-            
-            pokeImage.src = images[0]
-            
-            intervalId = setInterval(() => {
-               pokeImage.src = images[index]
-               index == 1 ? index = 0 : index++  
-            },1000)
-             
-        }
-        pokeImage.src = ''
+        let index = 0
+        let imagesURL = Object.values(pokemon.sprites)
+        let images = [imagesURL[6],imagesURL[2]]
+        
+        pokeImage.src = images[0]
+        
+        intervalId = setInterval(() => {
+           pokeImage.src = images[index]
+           index == 1 ? index = 0 : index++  
+        },1000)
+         
+    }
+    
+        
+        //pokeImage.src = ''
         clearInterval(intervalId)
-        changeImage()
+        
+        setTimeout(changeImage, 3000)
         
        
         nameAndId.innerHTML = `${pokemon.id} ${pokemon.name}`   
