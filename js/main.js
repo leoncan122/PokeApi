779fcfef15669edb518c6e2da01664dd4b87dbda
leoncan122 <leon.can122@gmail.com>
2021-04-19 16:29:26 +0200

    Slideshow pokemon

--- js/main.js
+++ js/main.js
@@ -24,40 +24,58 @@ var searchPokemonBtn = document.querySelector('#searchBtn')
 searchPokemonBtn.addEventListener('click', (event) => {
     event.preventDefault();
     
+
     var url = (`https://pokeapi.co/api/v2/pokemon/${newSearch.value}`)
     
 
     fetch(url)
     .then(response => response.json())
     .then( pokemon => {
-        console.log(pokemon)
+        
         //change values
-        // setTimeout([pokemon.sprites].forEach(img => {
-        //     index = 0
-        //     pokeImage.src = img 
-        //     index++
-        // }), 1000)
-        pokeImage.src = pokemon.sprites.front_default
+        var changeImage = function() {
+             
+            let index = 0
+            let imagesURL = Object.values(pokemon.sprites)
+            let images = [imagesURL[6],imagesURL[2]]
+
+            pokeImage.src = images[0]
+            
+            let id = setInterval(() => {
+               pokeImage.src = images[index]
+               index == 1 ? index = 0 : index++   
+            },1000)
+             
+        }
+        pokeImage.src = ''
+        changeImage()
+       
         nameAndId.innerHTML = `${pokemon.id} ${pokemon.name}`   
         weight.innerHTML = `weigth: <strong>0,${pokemon.weight} kg.</strong>`
         height.innerHTML = `height: ${pokemon.height}0 cm`
-        const abilitiesURL = pokemon.abilities.map(ability => fetch( ability.ability.url))
         pokemon.types.forEach( type => {
             typeField.innerHTML = type.type.name
         } )
+        
+        //const imagesURL = [Object.values(pokemon.sprites)]
+        const abilitiesURL = pokemon.abilities.map(ability => fetch( ability.ability.url))
         return Promise.all(abilitiesURL)
         
     })
     .then( (responses) => {
-        var promesas = responses.map( response => response.json())
+        
+        var promesas = responses.map(response => response.json())
+        // var images = responses[1].map(response => response.json())
+        
         return Promise.all(promesas)
     })
     .then( jsons => {
+       
         jsons.forEach(( ability) => {
-            
             descriptionField.innerText = ability.flavor_text_entries[0].flavor_text
-            
         })
+
     })
+    
   
 })
